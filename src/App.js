568341297fc9913820686cba420c86dbde1ea05b
ndt0153 <ndt0153@gmail.com>
2021-10-14 13:10:40 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,20 +9,15 @@ import Table from "./table";
 function App() {
   const [data, setData] = useState([]);
   const [pageCount, setPage] = useState(0);
+  const [acc, setAcc] = useState(0);
   const [DECPrice, setDECPrice] = useState(0);
+  const [totalDEC, setDEC] = useState(0);
+  const [totalPower, setPower] = useState(0);
 
-  const totalPowerCaculator = async (raw) => {
-    let totalDEC = 0;
-    raw.forEach(function (ele) {
-      totalDEC += ele[4];
-    });
-    return parseInt(totalDEC);
-  };
   const getDECPrice = async (raw) => {
     let result = await axios.get(
       "https://api.pancakeswap.info/api/v2/tokens/0xe9d7023f2132d55cbd4ee1f78273cb7a3e74f10a"
     );
-    //console.log(result);
     return result !== undefined
       ? setDECPrice(parseFloat(result.data.data.price))
       : 0;
@@ -102,9 +97,15 @@ function App() {
   };
   const fetchData = async (pageIndex) => {
     const userList = await axios.get("http://localhost:2000/b");
-    console.log(userList.data.products);
+    const totalDEC = await axios.get("http://localhost:2000/totalDEC");
+    const totalPower = await axios.get("http://localhost:2000/totalPower");
+    getDECPrice();
+
     setData(userList.data.products);
     setPage(userList.data.page);
+    setAcc(userList.data.count);
+    setDEC(totalDEC.data.total);
+    setPower(totalPower.data.total);
   };
   //const data2 = React.useMemo(() => getData2(), []);
   useEffect(() => {}, []);
@@ -113,19 +114,21 @@ function App() {
       <div className="grid grid-cols-4 gap-4 text-center my-8  items-center">
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg py-8">
           <p className="text-sm text-gray-500">Total Acc</p>
-          <h1 className="text-4xl text-green-500">{}</h1>
+          <h1 className="text-4xl text-green-500">{acc}</h1>
         </div>
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg py-8">
           <p className="text-sm text-gray-500">Total DEC</p>
-          <h1 className="text-4xl text-green-500">{}</h1>
+          <h1 className="text-4xl text-green-500">{totalDEC}</h1>
         </div>
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg py-8">
           <p className="text-sm text-gray-500">Total Power</p>
-          <h1 className="text-4xl text-green-500">{}</h1>
+          <h1 className="text-4xl text-green-500">{totalPower}</h1>
         </div>
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg py-8">
           <p className="text-sm text-gray-500">Total USD</p>
-          <h1 className="text-4xl text-green-500">{} USD</h1>
+          <h1 className="text-4xl text-green-500">
+            {parseFloat(DECPrice * totalDEC).toFixed(2)} USD
+          </h1>
         </div>
       </div>
       <Table
